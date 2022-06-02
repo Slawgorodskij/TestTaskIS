b3a90fe5c0c263583a3df9877bd6998846c6eed2
Slawgorodskij <Slawgorodskij@mail.ru>
2022-06-02 17:02:05 +0700

    add Data Base

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'collection_id'=>rand(1,6),
+            'name'=>$this->faker->word(),
+            'price'=>rand(500, 2000),
+        ];
+    }
+}
--- database/seeders/CollectionSeeder.php
+++ database/seeders/CollectionSeeder.php
@@ -2,21 +2,19 @@
 
 namespace Database\Seeders;
 
+use App\Models\Collection;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class CollectionSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call([
-            CollectionSeeder::class,
-            ProductSeeder::class,
-        ]);
+        Collection::factory(6)->create();
     }
 }
